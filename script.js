2a883b153fcad35f598b9b06a3f0a1673c3bf8bd
dvke <uchennaobiekwe@yahoo.com>
2022-10-24 23:33:01 +0100

    add click/display functionality

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
+let numbers =  document.querySelectorAll('.number');
+let operators = document.querySelector('.operator');
+let clearBtn = document.querySelector('.is-clear');
+let display = document.querySelector('.display');
+let calcInputDisplay = document.querySelector('.calc-input');
 
+
+function Type(button) {
+    calcInputDisplay.value = calcInputDisplay.value + button.innerHTML;
+}
+
+function Clear(){
+    calcInputDisplay.value = '';
+}
+
+numbers.forEach(n=>{
+    n.addEventListener('click', () => Type(n));
+})
+
+clearBtn.onclick = Clear;
 
