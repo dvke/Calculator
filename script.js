1e1ebf9c03d672a4b178b2e8fe2fbe7e8055d1e0
dvke <uchennaobiekwe@yahoo.com>
2022-10-25 14:42:43 +0100

    reworking the script

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-let numbers =  document.querySelectorAll('.number');
-let operators = document.querySelector('.operator');
-let clearBtn = document.querySelector('.is-clear');
-let display = document.querySelector('.display');
-let calcInputDisplay = document.querySelector('.calc-input');
+let calcBtns = document.querySelectorAll('.calc-button');
+let calcDisplay = document.querySelector('.display');
+let calcInput = document.querySelector('.calc-input');
 
 
-function Type(button) {
-    calcInputDisplay.value = calcInputDisplay.value + button.innerHTML;
-}
 
 function Clear(){
-    calcInputDisplay.value = '';
 }
 
-numbers.forEach(n=>{
-    n.addEventListener('click', () => Type(n));
-})
+function Display(){
+}
 
-clearBtn.onclick = Clear;
+function Operate(){
+}
+
+
 
